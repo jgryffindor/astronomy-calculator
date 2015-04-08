945292854766c01e9c6c7ade8e9a53253c8a2815
Justin Griffin <justin@justingriffin.biz>
2015-04-08 11:49:46 -0500

    Added main.cpp, updated changelog.

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+	0.1.0 - Initial commit
+
+	* -> Security Fix
+	# -> Bug Fix
+	$ -> Language fix or change
+	+ -> Addition
+	^ -> Change
+	- -> Removed
+	! -> Note
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  astronomy-calculator
+//
+//  Created by Justin on 4/8/15.
+//  Copyright (c) 2015 Justin. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
